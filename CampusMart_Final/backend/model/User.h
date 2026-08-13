ae945f2a36bff888df17a58040242e08ecf7174d
Rahul M <rahullw2007@gmail.com>
2026-08-13 13:09:58 +0530

    Delete CampusMart_Final directory

--- CampusMart_Final/backend/model/User.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-struct User {
-    int id{};
-    std::string name;
-    std::string email;
-    std::string role;
-};
--- CampusMart_Final/backend/repository/README.md
+++
@@ -1,2 +0,0 @@
-Repository layer: database operations.
-See src/repository/.
